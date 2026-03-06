633b165acc1e7389d0cd1af775283711c4731ce7
jomadu <maxdunn123@gmail.com>
2026-03-05 20:52:15 -0800

    Implement ralph version subcommand (ralph-vvt, O4/R7)
    - Add cobra dependency
    - Create version subcommand that prints 'ralph 0.1.0'
    - Version appears in help output
    - Exits with code 0
    - No config required per spec

--- cmd/ralph/main.go
+++ cmd/ralph/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "ralph",
+	Short: "Ralph - a dumb loop that pipes prompts to AI CLIs",
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print version information",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("ralph 0.1.0")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
 
 func main() {
-	fmt.Println("ralph")
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
